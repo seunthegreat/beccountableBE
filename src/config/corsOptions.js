80e41f7319d5741a952e1cdda482f3764a3b6a81
sabilawon <sabilawon@turmsadrain.com>
2023-06-14 23:39:56 +0100

    no-referrer

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -8,7 +8,8 @@ const corsOptions = {
         callback(new Error('Not allowed by CORS'));
       }
     },
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  referrerPolicy: 'no-referrer'
 }
 
 module.exports = corsOptions;
