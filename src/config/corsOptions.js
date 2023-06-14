f4f797fd901ec4095e56adfc854a44b257eed6de
sabilawon <sabilawon@turmsadrain.com>
2023-06-15 00:31:54 +0100

    another one

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -16,6 +16,10 @@ const corsOptions = {
   exposedHeaders: ['Content-Type'],
   allowedHeaders: ['Content-Type', 'Authorization', '*'], // Include 'Authorization' header
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  frameguard: {
+    action: 'allow-from', // Set 'allow-from' to allow cross-origin framing
+    domain: 'https://beccountable-frontend.vercel.app' // Replace with the domain you want to allow framing from
+  },
 };
 
 module.exports = corsOptions;
