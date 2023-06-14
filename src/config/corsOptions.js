7739ec13e70b7b14a146c2fcd2865093d7dd7c0b
sabilawon <sabilawon@turmsadrain.com>
2023-06-15 00:22:44 +0100

    final update

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -3,16 +3,19 @@ const allowedOrigins = require('./allowedOrigins');
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+      callback(null, true);
     } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200,
   referrerPolicy: 'no-referrer',
   credentials: true,
   crossOriginOpenerPolicy: 'allow-from',
-  crossOriginResourcePolicy: 'allow-from'
+  crossOriginResourcePolicy: 'allow-from',
+  exposedHeaders: ['Content-Type'],
+  allowedHeaders: ['Content-Type', '*'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
 module.exports = corsOptions;
